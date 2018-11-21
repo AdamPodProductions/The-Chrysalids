39e4385d603e97bf973812e1c765ec975eca69d4
Adam Pod <adam.podoxin@gmail.com>
2018-11-20 16:25:14 -0800

    Created player health

--- Assets/Scripts/Battle/PlayerBattleMovement.cs
+++ Assets/Scripts/Battle/PlayerBattleMovement.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour
+public class PlayerBattleMovement : MonoBehaviour
 {
     public float speed = 2f;
 
---
+++ Assets/Scripts/Battle/PlayerHealth.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public int health = 20;
+
+    public Image healthImg;
+    public Text healthAmount;
+
+    public void SetHealth(int health)
+    {
+        this.health = health;
+
+        healthImg.fillAmount = health / 20f;
+        healthAmount.text = health + "/20";
+
+        if (health <= 0)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+    }
+
+    public void Damage(int damage)
+    {
+        SetHealth(health - damage);
+    }
+}
