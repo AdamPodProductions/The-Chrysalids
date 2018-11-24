84925f859baed43d09ca2a5d5d2703e6b9f5dcf8
Adam Pod <adam.podoxin@gmail.com>
2018-11-23 22:39:36 -0800

    Put rock sprite in game

---
+++ Assets/Scripts/EnemyTextBox.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EnemyTextBox : MonoBehaviour
+{
+    public string[] textToSay;
+    public float lettersPerSec;
+
+    public GameObject textBox;
+    public Text text;
+
+    private void Start()
+    {
+        Say();
+    }
+
+    private IEnumerator SayCoroutine()
+    {
+        string currentString = textToSay[Random.Range(0, textToSay.Length)];
+
+        for (int i = 0; i < currentString.Length; i++)
+        {
+            text.text += currentString[i];
+            yield return new WaitForSeconds(1f / lettersPerSec);
+        }
+
+        yield return new WaitForSeconds(1);
+
+        textBox.SetActive(false);
+    }
+
+    public void Say()
+    {
+        textBox.SetActive(true);
+        StartCoroutine(SayCoroutine());
+    }
+}
