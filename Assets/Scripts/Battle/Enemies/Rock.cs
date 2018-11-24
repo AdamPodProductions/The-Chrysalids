16b42868bc3e3644a86321c15b4fdf6db5c6618f
Adam Pod <adam.podoxin@gmail.com>
2018-11-24 08:49:33 -0800

    Added rock behaviour and music

---
+++ Assets/Scripts/Battle/Enemies/Rock.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rock : MonoBehaviour
+{
+    private IEnumerator AttackCoroutine()
+    {
+        yield return new WaitForSeconds(2);
+        BattleManager.instance.PlayerTurn();
+    }
+
+    public void Attack()
+    {
+        StartCoroutine(AttackCoroutine());
+    }
+}
