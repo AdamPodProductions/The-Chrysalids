12bd92777d8c9ce9f348c7f6119f815aa41e63a5
Adam Pod <adam.podoxin@gmail.com>
2018-12-05 19:39:58 -0800

    Creaetd player movement script

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,7 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour 
+public class PlayerMovement : MonoBehaviour
 {
-	
+    public float speed = 5f;
+
+    private Vector2 movement;
+
+    private void Update()
+    {
+        movement = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        transform.Translate(movement * speed * Time.fixedDeltaTime);
+    }
 }
