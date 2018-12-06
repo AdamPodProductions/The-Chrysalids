c804ed7c13a79c2f83fb83cbe38ff3c8dce3569b
Adam Pod <adam.podoxin@gmail.com>
2018-12-05 20:15:55 -0800

    Added David animations

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,10 +7,24 @@ public class PlayerMovement : MonoBehaviour
     public float speed = 5f;
 
     private Vector2 movement;
+    private bool isMoving;
+
+    private Animator animator;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
 
     private void Update()
     {
         movement = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        isMoving = movement != Vector2.zero;
+
         transform.Translate(movement * speed * Time.fixedDeltaTime);
+
+        animator.SetBool("isMoving", isMoving);
+        animator.SetFloat("xMove", movement.x);
+        animator.SetFloat("yMove", movement.y);
     }
 }
