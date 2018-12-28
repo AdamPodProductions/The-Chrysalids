d692e8d89da31859d6ced3be1dd1f828ffd94df9
Adam Pod <adam.podoxin@gmail.com>
2018-12-28 11:32:15 -0800

    Created interactable

---
+++ Assets/Scripts/Interactable.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interactable : MonoBehaviour
+{
+    public string[] textToSay;
+
+    private TalkingTextBox talkingTextBox;
+
+    private void Start()
+    {
+        talkingTextBox = FindObjectOfType<TalkingTextBox>();
+    }
+
+    public void Interact()
+    {
+        talkingTextBox.textToSay = textToSay;
+        talkingTextBox.Say();
+    }
+}
---
+++ Assets/Scripts/Player/PlayerInteraction.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInteraction : MonoBehaviour
+{
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (collision.tag == "Interactable")
+            {
+                collision.GetComponent<Interactable>().Interact();
+            }
+        }
+    }
+}
