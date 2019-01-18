7150c337b71aacde04fdfcd713ba62a87819ced3
Adam Pod <adam.podoxin@gmail.com>
2019-01-17 21:02:48 -0800

    Created fade

---
+++ Assets/Scripts/Fade.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Fade : MonoBehaviour
+{
+    public string levelToLoad;
+
+    public void FadeOut()
+    {
+        GetComponent<Animator>().Play("FadeOut");
+    }
+
+    public void ChangeLevel()
+    {
+        SceneManager.LoadScene(levelToLoad);
+    }
+}
--- Assets/Scripts/Player/PlayerInteraction.cs
+++ Assets/Scripts/Player/PlayerInteraction.cs
@@ -11,6 +11,15 @@ public class PlayerInteraction : MonoBehaviour
         talkingTextBox = FindObjectOfType<TalkingTextBox>();
     }
 
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("FadeEdge"))
+        {
+            FindObjectOfType<PlayerMovement>().canMove = false;
+            FindObjectOfType<Fade>().FadeOut();
+        }
+    }
+
     private void OnTriggerStay2D(Collider2D collision)
     {
         if (Input.GetKeyDown(KeyCode.Space))
